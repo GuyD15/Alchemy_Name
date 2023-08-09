fab8757d830bcb30bba42f060cb8e8f7837d51c5
Abdikalik Dini <adini18@gmail.com>
2023-08-09 11:59:50 -0400

    defined route for elements api

--- controllers/elements-routes.js
+++ controllers/elements-routes.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+// Define route handlers
+router.get('/', (req, res) => {
+  res.send('elements API');
+});
+
+router.get('/element/:id', (req, res) => {
+
+});
+
+
+module.exports = router;
