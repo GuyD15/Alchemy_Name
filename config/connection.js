5df9e2981456e7a5d3312d8125662ee33e15479f
Mona Lisa <krmproductions19@gmail.com>
2023-08-10 19:50:55 -0400

    Server running

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,16 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3001,
+    port: 3306,
 });
 
-sequelize
-.authenticate()
-.then(() => {
-    console.log('Connection has been established')
-})
-.catch(err => {
-    console.error('Unable to connect.', err);
-});
+// sequelize
+// .authenticate()
+// .then(() => {
+//     console.log('Connection has been established')
+// })
+// .catch(err => {
+//     console.error('Unable to connect.', err);
+// });
 
-module.exports=sequelize;
+module.exports = sequelize;
--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const router = require('express').Router();
+const apiRoutes = require('./api')
+const homeRoutes = require('./home-route');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
+module.exports = router;
+
