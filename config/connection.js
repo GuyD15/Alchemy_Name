c0ad33b7a5ad4a5b3123a38a2f0458d48d097cbe
GuyD15 <guywayne.domino7@gmail.com>
2023-08-09 19:19:18 -0500

    Wrote connection.js file and added schema.sql file

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+sequelize
+.authenticate()
+.then(() => {
+    console.log('Connection has been established')
+})
+.catch(err => {
+    console.error('Unable to connect.', err);
+});
+
+module.exports=sequelize;
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE numbers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    value INT NOT NULL
+);
