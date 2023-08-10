3a6db09e23902b6c4030fc9ee1b9cb48537e8ba5
Jack <131559096+jackjg123@users.noreply.github.com>
2023-08-09 21:51:40 -0400

    updated Numbers.js

--- Models/Numbers.js
+++ Models/Numbers.js
@@ -3,17 +3,27 @@ const sequelize = require('../config/connection');
 
 class Number extends Model {}
 Number.init(
-    {
-        value: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },    
+  {
+    value: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
-    {
-        sequelize,
-        modelName: 'Number',
-        tableName: 'numbers'
-    }
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    is_even: {
+      type: DataTypes.INTEGER,
+      get() {
+        return this.value % 2 === 0;
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Number',
+    tableName: 'numbers',
+  }
 );
 
 module.exports = Number;
