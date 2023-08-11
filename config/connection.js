52ea0c96c5a87934898083a6300a17220be0dcea
GuyD15 <guywayne.domino7@gmail.com>
2023-08-10 19:54:07 -0500

    Fixed dotenv section of server.js

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'localhost',
@@ -5,13 +6,13 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
     port: 3306,
 });
 
-// sequelize
-// .authenticate()
-// .then(() => {
-//     console.log('Connection has been established')
-// })
-// .catch(err => {
-//     console.error('Unable to connect.', err);
-// });
+sequelize
+.authenticate()
+.then(() => {
+    console.log('Connection has been established')
+})
+.catch(err => {
+    console.error('Unable to connect.', err);
+});
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const routes = require('./controllers')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+require('dotenv').config();
+
+if (!process.env.DB_USERNAME || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
+  throw new Error('Required environment variables are missing. Make sure to create a .env file.');
+}
+
+
 // Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
