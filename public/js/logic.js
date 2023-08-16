59ac7b54301ead73c5c2902e2d9b80ec7c11f886
GuyD15 <guywayne.domino7@gmail.com>
2023-08-15 21:09:44 -0500

    Changed logic code to add reset button for the gmae. Also added some styling

--- public/js/logic.js
+++ public/js/logic.js
@@ -2,34 +2,54 @@ document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.getElementById('start-button');
     const numberContainer = document.getElementById('number-container');
     const dropZone = document.getElementById('drop-zone');
-
+    const resetButton = document.getElementById('reset-button');
+    
+    let totalScore = 0;
+  
+    function displayNumbers() {
+      for (let i = 1; i <= 10; i++) {
+        const number = document.createElement('div');
+        number.classList.add('draggable-number');
+        number.textContent = i;
+        number.setAttribute('draggable', 'true');
+  
+        numberContainer.appendChild(number);
+  
+        number.addEventListener('dragstart', (e) => {
+          e.dataTransfer.setData('text/plain', e.target.textContent);
+        });
+      }
+    }
+  
     startButton.addEventListener('click', () => {
-        startButton.remove();
-
-        for (let i = 1; i <= 10; i++) {
-            const number = document.createElement('div');
-            number.classList.add('draggable-number');
-            number.textContent = i;
-            number.setAttribute('draggable', 'true');
-
-            numberContainer.appendChild(number);
-
-            number.addEventListener('dragstart', (e) => {
-                e.dataTransfer.setData('text/plain', e.target.textContent);
-            });
-        }
-        dropZone.addEventListener('dragover', (e) => {
-            e.preventDefault(); 
-        });
-        
-
-        dropZone.addEventListener('drop', (e) => {
-            e.preventDefault();
-
-            const draggedNumber = e.dataTransfer.getData('text/plain');
-            const existingTotal = parseInt(dropZone.textContent) || 0;
-            const newTotal = existingTotal + parseInt(draggedNumber);
-            dropZone.textContent = newTotal;
-        });
+      startButton.remove();
+      displayNumbers();
+      
+      // Rest of the existing code
+  
+      dropZone.addEventListener('dragover', (e) => {
+        e.preventDefault();
+      });
+  
+      dropZone.addEventListener('drop', (e) => {
+        e.preventDefault();
+  
+        const draggedNumber = e.dataTransfer.getData('text/plain');
+        const existingTotal = totalScore;
+        totalScore = existingTotal + parseInt(draggedNumber);
+        dropZone.textContent = totalScore;
+      });
     });
-});
+  
+    resetButton.addEventListener('click', () => {
+      totalScore = 0; // Reset the score only
+  
+      // You might also want to clear the drop zone here if needed
+      dropZone.textContent = '0';
+    });
+  });
+  
+  
+  
+  
+  
