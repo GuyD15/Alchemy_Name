784771ac901c22d96e0eef383e7f260b04bdf706
Abdikalik Dini <adini18@gmail.com>
2023-08-16 19:46:27 -0400

    added number function

--- public/js/logic.js
+++ public/js/logic.js
@@ -4,73 +4,71 @@ document.addEventListener('DOMContentLoaded', () => {
   const dropZone = document.getElementById('drop-zone');
   const resetButton = document.getElementById('reset-button');
   const timerDisplay = document.getElementById('timer');
-
+  const targetDisplay = document.getElementById('target-number');
+  const scoreDisplay = document.getElementById('score');
   let totalScore = 0;
-  let timerInterval; // Keep a reference to the timer interval
-
-  // Displays the draggable numbers when start button is clicked.
+  let timerInterval;
+  let targetNumber;
+  let currentScore = 0;
   function displayNumbers() {
     for (let i = 1; i <= 10; i++) {
       const number = document.createElement('div');
       number.classList.add('draggable-number');
       number.textContent = i;
       number.setAttribute('draggable', 'true');
-
       numberContainer.appendChild(number);
-
-      // Allows user to drag numbers
       number.addEventListener('dragstart', (e) => {
         e.dataTransfer.setData('text/plain', e.target.textContent);
       });
     }
   }
-
-  // Timer function
+  function generateTargetNumber() {
+    return Math.floor(Math.random() * 55) + 1;
+  }
   function startTimer(duration, display) {
-    clearInterval(timerInterval); 
-
+    clearInterval(timerInterval);
     let timer = duration;
     timerInterval = setInterval(function () {
       let minutes = parseInt(timer / 60, 10);
       let seconds = parseInt(timer % 60, 10);
-
       minutes = minutes < 10 ? "0" + minutes : minutes;
       seconds = seconds < 10 ? "0" + seconds : seconds;
-
       display.textContent = minutes + ":" + seconds;
-
       if (--timer < 0) {
-        timer = duration;
+        clearInterval(timerInterval);
       }
     }, 1000);
   }
-
+  function resetRound() {
+    clearInterval(timerInterval);
+    totalScore = 0;
+    dropZone.textContent = '0';
+    startTimer(60, timerDisplay);
+    targetNumber = generateTargetNumber();
+    targetDisplay.textContent = "Target: " + targetNumber;
+  }
   startButton.addEventListener('click', () => {
     startButton.remove();
     displayNumbers();
-    startTimer(60, timerDisplay); // Start timer for 60 seconds
-
-  
-    // Allows user to drag number to the dropzone.
-    dropZone.addEventListener('dragover', (e) => {
-      e.preventDefault();
-    });
-
-    // Allows user to drop numbers into drop zone.
-    dropZone.addEventListener('drop', (e) => {
-      e.preventDefault();
-
-      // Adds draggedNumber value to current score.
-      const draggedNumber = e.dataTransfer.getData('text/plain');
-      const existingTotal = totalScore;
-      totalScore = existingTotal + parseInt(draggedNumber);
-      dropZone.textContent = totalScore;
-    });
+    startTimer(60, timerDisplay);
+    targetNumber = generateTargetNumber();
+    targetDisplay.textContent = "Target: " + targetNumber;
   });
-
-  resetButton.addEventListener('click', () => {
-    totalScore = 0; // Reset the score only
-    dropZone.textContent = '0'; // Clear the drop zone
-    startTimer(60, timerDisplay); // Restart the timer for 60 seconds
+  dropZone.addEventListener('dragover', (e) => {
+    e.preventDefault();
   });
+  dropZone.addEventListener('drop', (e) => {
+    e.preventDefault();
+    const draggedNumber = e.dataTransfer.getData('text/plain');
+    totalScore += parseInt(draggedNumber);
+    dropZone.textContent = totalScore;
+    if (totalScore === targetNumber) {
+        currentScore++;
+        scoreDisplay.textContent = "Score: " + currentScore;
+        resetRound();
+    } else if (totalScore > targetNumber) {
+        resetRound(); // If total score exceeds target, reset the round.
+    }
+  });
+  resetButton.addEventListener('click', resetRound);
 });
