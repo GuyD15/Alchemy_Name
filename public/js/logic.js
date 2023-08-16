91a4a72431d44e2be8c1e1a126403b669eb29bf5
Abdikalik Dini <adini18@gmail.com>
2023-08-16 19:10:12 -0400

    added timer

--- public/js/logic.js
+++ public/js/logic.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const numberContainer = document.getElementById('number-container');
   const dropZone = document.getElementById('drop-zone');
   const resetButton = document.getElementById('reset-button');
+  const timerDisplay = document.getElementById('timer');
 
   let totalScore = 0;
+  let timerInterval; // Keep a reference to the timer interval
 
   // Displays the draggable numbers when start button is clicked.
   function displayNumbers() {
@@ -23,17 +25,38 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  // Timer function
+  function startTimer(duration, display) {
+    clearInterval(timerInterval); 
+
+    let timer = duration;
+    timerInterval = setInterval(function () {
+      let minutes = parseInt(timer / 60, 10);
+      let seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+        timer = duration;
+      }
+    }, 1000);
+  }
+
   startButton.addEventListener('click', () => {
     startButton.remove();
     displayNumbers();
+    startTimer(60, timerDisplay); // Start timer for 60 seconds
 
-    // Rest of the existing code
+  
     // Allows user to drag number to the dropzone.
     dropZone.addEventListener('dragover', (e) => {
       e.preventDefault();
     });
 
-    //Allows user to drop numbers into drop zone.
+    // Allows user to drop numbers into drop zone.
     dropZone.addEventListener('drop', (e) => {
       e.preventDefault();
 
@@ -47,8 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   resetButton.addEventListener('click', () => {
     totalScore = 0; // Reset the score only
-
-    // You might also want to clear the drop zone here if needed
-    dropZone.textContent = '0';
+    dropZone.textContent = '0'; // Clear the drop zone
+    startTimer(60, timerDisplay); // Restart the timer for 60 seconds
   });
 });
