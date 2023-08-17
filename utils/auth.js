2c9affdd08a775e949498a932db87de8976030d5
Jack <131559096+jackjg123@users.noreply.github.com>
2023-08-17 19:54:30 -0400

    Adjusted styling for drop zone, reset button, draggable numbers, fonts, and added link in main.handlebars.

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,11 @@
 const withAuth = (req, res, next) => {
   if (!req.session.logged_in) {
+    console.log('User is not logged in. Redirecting to /sign-in');
     res.redirect('/sign-in');
   } else {
+    console.log(
+      'User is logged in. Proceeding to the next middleware or route.'
+    );
     next();
   }
 };
